c373b968c4018a43eaded26c4cd625dfac36ce2e
AdrianMargel <carbongrouplife@gmail.com>
2019-01-15 16:47:43 -0700

    Create turn.cs

---
+++ reviewsolution/oopsreiview/turn.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace oopsreiview
+{
+    class turn
+    {
+        private int p1Roll;
+        private int p2Roll;
+        private Die p1Die;
+        private Die p2Die;
+        turn()
+        {
+
+        }
+        public bool p1Win()
+        {
+            return p1Roll > p2Roll;
+        }
+    }
+}
