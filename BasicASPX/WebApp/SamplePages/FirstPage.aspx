f27cea7596e44f3eea56e9c20fd211455f56a09e
AdrianMargel <carbongrouplife@gmail.com>
2019-02-26 16:51:35 -0700

    ASP example started

---
+++ BasicASPX/WebApp/SamplePages/FirstPage.aspx
@@ -0,0 +1,14 @@
+﻿<%@ Page Title="First Page" Language="C#" MasterPageFile="~/Site.Master" AutoEventWireup="true" CodeBehind="FirstPage.aspx.cs" Inherits="WebApp.SamplePages.FirstPage" %>
+<asp:Content ID="Content1" ContentPlaceHolderID="MainContent" runat="server">
+
+    <asp:Label ID="Label1" runat="server" Text="Enter your name"></asp:Label>
+    &nbsp&nbsp;
+
+    <asp:TextBox ID="yourname" runat="server" OnTextChanged="TextBox1_TextChanged"></asp:TextBox>
+    <br />
+    <asp:Button ID="PressMe" runat="server" Text="I'm so o-pressed" OnClick="PressMe_Click" />
+    <br />
+    <asp:Literal ID="OutputMessage" runat="server"></asp:Literal>
+    <p>
+        &nbsp;</p>
+</asp:Content>
---
+++ BasicASPX/WebApp/SamplePages/FirstPage.aspx.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace WebApp.SamplePages
+{
+    public partial class FirstPage : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            //this method will execute each time this page is processed
+            //it will execute before any event method
+        }
+
+        protected void TextBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void PressMe_Click(object sender, EventArgs e)
+        {
+            OutputMessage.Text= "hello"+yourname.Text;
+        }
+    }
+}
